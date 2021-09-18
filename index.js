17a2947483db7f045d15245002ad45081eda6108
s-vamshi <62489114+s-vamshi@users.noreply.github.com>
2021-09-18 06:05:12 +0530

    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+const result = document.getElementById('result');
+const presentList = [];
+const btn = document.querySelectorAll('.btn').forEach(item => {
+    item.addEventListener('click', event => {
+        if(item.classList.contains("btn-danger")){
+            item.classList.remove("btn-danger");
+            item.classList.add("btn-success");
+            presentList.push(item.innerHTML);
+            
+        }
+        else{
+            presentList2.pop(item.innerHTML);
+            item.classList.remove("btn-success");
+            item.classList.add("btn-danger");
+        }
+        presentList.sort();
+        result.innerText=presentList;
+        // location.href='#result';
+    })
+})
+result.addEventListener('click', event =>{
+    console.log(this);
+    result.select();
+    document.execCommand('copy');
+    alert("COPIED!");
+});
