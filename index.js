3185835052386ceb8140838e10e13325f30b9441
s-vamshi <62489114+s-vamshi@users.noreply.github.com>
2021-09-18 13:02:57 +0530

    strength added in index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const result = document.getElementById('result');
 const presentList = [];
+let strength = document.getElementById('strength');
+let len = presentList.length;
 const btn = document.querySelectorAll('.btn').forEach(item => {
     item.addEventListener('click', event => {
         if(item.classList.contains("btn-danger")){
             item.classList.remove("btn-danger");
             item.classList.add("btn-success");
             presentList.push(item.innerHTML);
-            
         }
         else{
             presentList.pop(item.innerHTML);
@@ -14,12 +15,13 @@ const btn = document.querySelectorAll('.btn').forEach(item => {
             item.classList.add("btn-danger");
         }
         presentList.sort();
+        len=presentList.length;
         result.innerText=presentList;
+        strength.innerText='Total no. of students present:  '+len;
         // location.href='#result';
     })
 })
 result.addEventListener('click', event =>{
-    console.log(this);
     result.select();
     document.execCommand('copy');
     alert("COPIED!");
